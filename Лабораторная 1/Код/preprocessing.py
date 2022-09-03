0900e499c075fbb787737d05735ab6a70e01e96c
karicotiza <Kur7umerovD@Gmail.com>
2022-09-03 18:23:01 +0300

    Добавил проверку на тип страницы и символы

--- Лабораторная 1/Код/preprocessing.py
+++ Лабораторная 1/Код/preprocessing.py
@@ -1,4 +1,5 @@
 import nltk
+import string
 
 
 def normalize(sentence: str) -> list:
@@ -12,7 +13,24 @@ def normalize(sentence: str) -> list:
         word for word in sentence if nltk.pos_tag([word])[0][1] in valid_parts_of_speech
     ]
     sentence = [lemmatizer.lemmatize(word).lower() for word in sentence]
+    sentence = [word for word in sentence if is_in_english(word)]
 
     return sentence
 
 
+def is_article(title: str) -> bool:
+    if not title.startswith("User:") and not title.startswith("Talk:"):
+        return True
+    else:
+        return False
+
+
+def is_in_english(word: str) -> bool:
+    allowed_letters = list(string.ascii_lowercase)
+    allowed_letters.append("'")
+    for letter in word.lower():
+        if letter in allowed_letters:
+            pass
+        else:
+            return False
+    return True
